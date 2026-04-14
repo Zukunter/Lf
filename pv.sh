d527c386b0895d6dbdd5b56aa1de8f7118a805fc
Zukunter <Zukunftsdeuter@gmail.com>
2026-04-14 16:50:40 -0300

    Upgrade Preview

--- pv.sh
+++ pv.sh
@@ -3,13 +3,4 @@
 # Die Datei, die gerade ausgewählt ist / The currently selected file
 file=$1
 
-case "$file" in
-    # Nutze bat für alle Textdateien / Use bat for all text files
-    *.zig|*.rs|*.cs|*.lua|*.sh|*.conf|*.json|*.md)
-        bat --color=always --style=numbers --terminal-width="$2" "$file"
-        ;;
-    *)
-        # Standard-Vorschau für alles andere / Default preview for everything else
-        cat "$file"
-        ;;
-esac
+bat --color=always --style=numbers --terminal-width="$2" "$file"
